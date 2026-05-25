400227a5986ffcf75ecbe77a3563e8579352602a
gp-kyouma <112660601+gp-kyouma@users.noreply.github.com>
2026-05-25 01:41:00 +0000

    minimap dummy

--- frontend/src/components/GameScreen.jsx
+++ frontend/src/components/GameScreen.jsx
@@ -1,5 +1,22 @@
 import { useState } from 'react';
 
+function Minimap(mapData, viewData) {
+
+    let data = [[1,2,3],[4,5,6],[7,8,9]]
+
+    return (
+        <table class="minimap-grid">
+            {data.map(item => 
+                <tr>
+                    {item.map(i => 
+                    <td>
+                        <div>{i}</div>
+                    </td>)}
+                </tr>)}
+        </table>
+    );
+}
+
 export default function GameScreen({data}) {
 
     return (
@@ -8,6 +25,7 @@ export default function GameScreen({data}) {
             hello i am a placeholder here is my data: 
             {JSON.stringify(data,null,2)}
         </h2>
+        <Minimap/>
         </>
     );
 }
