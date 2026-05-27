19e9ac879f4806b3deca40958ac975a4348d88da
gp-kyouma <112660601+gp-kyouma@users.noreply.github.com>
2026-05-27 20:21:13 +0000

    minimap dummy++, log dummy

--- frontend/src/components/GameScreen.jsx
+++ frontend/src/components/GameScreen.jsx
@@ -2,11 +2,41 @@ import { useState } from 'react';
 
 function MinimapSquare(data) {
 
-    //placeholder testing
     let classtype = "no-tile"
 
-    if (data % 2){
+    // placeholder, testing all styles
+    if (data == 0){
+        classtype = "no-tile"
+    }
+    else if (data == 1){
         classtype = "unknown-tile"
+        data = "?"
+    }
+    else{
+        if ((data%10) < 5){
+            classtype = "visited-tile-"
+        }
+        else{
+            classtype = "current-tile-"
+        }
+
+        switch (data % 5) {
+            case 0:
+                classtype = classtype + "default"
+                break;
+            case 1:
+                classtype = classtype + "treasure"
+                break;
+            case 2:
+                classtype = classtype + "shop"
+                break;
+            case 3:
+                classtype = classtype + "boss"
+                break;
+            case 4:
+                classtype = classtype + "safe"
+                break;
+        }
     }
 
     return (
@@ -16,29 +46,67 @@ function MinimapSquare(data) {
     );
 }
 
-function Minimap(mapData, viewData) {
+function Minimap({mapData, viewData}) {
 
-    let data = [[1,2,3],[4,5,6],[7,8,9]]
+    let data = []
+
+    //fake data for testing
+    for (let i = 0; i < 9; i++) {
+        data[i] = []; // Initialize inner array for the row
+        for (let j = 0; j < 9; j++) {
+            data[i][j] = i*9+j;
+        }
+    }
 
     return (
-        <table class="minimap-grid">
-            {data.map(item => 
-                <tr>
-                    {item.map(i => MinimapSquare(i))}
-                </tr>)}
-        </table>
+        <div class='child flex-child'>
+            <p></p>
+            <table class="minimap-grid">
+                {data.map(item => 
+                    <tr>
+                        {item.map(i => MinimapSquare(i))}
+                    </tr>)}
+            </table>
+        </div>
+    );
+}
+
+function Log({logData}) {
+    
+    return (
+        <div class='child flex-child'>
+            <p></p>
+            <ul class="scroll-list">
+                {logData.map(i => <li>{i}</li>)}
+            </ul>
+        </div>
     );
 }
 
 export default function GameScreen({data}) {
 
+    const [log, setLog] = useState(["first"]);
+
+    function addToLog(newText) {
+        setLog([...log, newText]);
+    }
+
     return (
         <>
+        <div class='parent flex-parent'>
+            <Log logData = {log}/>
+            <Minimap/>
+        </div>
+
         <h2>
-            hello i am a placeholder here is my data: 
+            hello i am a placeholder here is my data from start screen: 
+            <br/>
             {JSON.stringify(data,null,2)}
         </h2>
-        <Minimap/>
+
+        <button type="button" onClick={() => addToLog("new line")}>
+            add line to log
+        </button>
         </>
     );
 }
